6520162e81147305ef453ebaa88f1d0aa6dc500e
Piyali Gupta <Piyali.Gupta@alaskaair.com>
2023-10-10 16:56:50 +0530

    modified

---
+++ ToDoList_ServiceBus/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+
+[assembly: FunctionsStartup(typeof(FetchToDoListFunc.Startup))]
+namespace FetchToDoListFunc
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Access the configuration
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
